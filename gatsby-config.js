bf5813789238558eefc4e16b518573cff0d4723c
hajzeer <krzysh98@gmail.com>
2020-11-25 21:32:24 +0100

    template commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Finansowi Eksperci | Twoje wsparcie w oddłużeniu`,
+    title: `Eksperci Finansowi | Twoje wsparcie w oddłużeniu`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
