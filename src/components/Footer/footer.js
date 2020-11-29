96599a4bd763783af28ebf2a4db11e5f7d439603
hajzeer <krzysh98@gmail.com>
2020-11-29 19:33:33 +0100

    prefinish app

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -4,12 +4,12 @@ import styles from "./footer.module.css";
 const footer = () => {
     const website = "https://hitherestudio.netlify.app/pl/"
     return (
-        <section className={styles.footer}>
+        <div className={styles.footer}>
             <p className={styles.paragraph}>Wszelkie prawa zastrzeżone</p>
             <p className={styles.paragraph}>© 2020 Designed and created by <br/>
                 <a className={styles.anchor} href={website}> hiThere Studio</a>
             </p>
-        </section>
+        </div>
     )
 }
 export default footer
--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -5,14 +5,34 @@
  * See: https://www.gatsbyjs.com/docs/use-static-query/
  */
 
-import React from "react"
+import React, {useEffect} from "react"
 import PropTypes from "prop-types"
+import gsap from "gsap"
+import {ScrollTrigger} from "gsap/ScrollTrigger";
 
 import Header from "../Header/header"
 import Footer from "../Footer/footer"
 import "./layout.module.css"
 
 const Layout = ({ children }) => {
+
+    useEffect(() => {
+
+        const sections = document.querySelectorAll('section');
+        gsap.registerPlugin(ScrollTrigger)
+
+        sections.forEach(section => {
+            gsap.fromTo(section,
+                {opacity: 0},
+                {opacity: 1, stagger: 0.4, duration: 1, ease: 'power4.Out',
+                    scrollTrigger: {
+                        trigger: section,
+                        start: 'top 75%',
+                    }});
+        });
+
+    })
+
   return (
     <>
       <Header />
